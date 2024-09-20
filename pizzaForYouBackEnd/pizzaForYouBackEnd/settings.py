0a2132c00217c35bf667ee9f05510aceaaf6808f
ramon <lionlightheart@gmail.com>
2024-09-20 22:04:32 +0200

    Refactor project settings and URL configuration

--- pizzaForYouBackEnd/pizzaForYouBackEnd/settings.py
+++ pizzaForYouBackEnd/pizzaForYouBackEnd/settings.py
@@ -22,6 +22,7 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(Path(__file__).resolve().parent.parent,  '..','.env'))
+DEBUG = bool(os.getenv('DEBUG', False))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
--- pizzaForYouBackEnd/pizzaForYouBackEnd/urls.py
+++ pizzaForYouBackEnd/pizzaForYouBackEnd/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.http import HttpResponseForbidden
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/', include('users.urls')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns[0] = path('admin/', lambda request: HttpResponseForbidden("Forbidden: admin access disabled in production!"))
