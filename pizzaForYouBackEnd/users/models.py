bd2a13f3646067ce00e231f5b31800ed8a9db464
ramon <lionlightheart@gmail.com>
2024-09-20 10:39:09 +0200

    Refactor project structure and add Django files

--- pizzaForYouBackEnd/users/models.py
+++ pizzaForYouBackEnd/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+    
