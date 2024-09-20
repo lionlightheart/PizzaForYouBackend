75d55cc7d698f44d2558c7e772ac27e042eb5a44
ramon <lionlightheart@gmail.com>
2024-09-20 21:53:23 +0200

    Refactor project structure and add roles app

--- pizzaForYouBackEnd/pizzaForYouBackEnd/urls.py
+++ pizzaForYouBackEnd/pizzaForYouBackEnd/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('admin/', admin.site.urls),
+    # path('api/', include('users.urls')),
 ]
