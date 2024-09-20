9265889962a0c8fc2230c3f5156072b2116b0759
ramon <lionlightheart@gmail.com>
2024-09-20 11:47:29 +0200

    Refactor project structure and add roles app

--- pizzaForYouBackEnd/roles/models.py
+++ pizzaForYouBackEnd/roles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Role(models.Model):
+    role_id = models.UUIDField(primary_key=True, editable=False, unique=True, default=uuid.uuid4)
+    role_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.role_name
--- pizzaForYouBackEnd/users/models.py
+++ pizzaForYouBackEnd/users/models.py
@@ -1,14 +1,14 @@
 import uuid
 from django.db import models
-
+from roles.models import Role
 
 class User(models.Model):
     user_id = models.UUIDField(primary_key=True, editable=False, unique=True, default=uuid.uuid4)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    role = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    rol= models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True, related_name='roles') 
 
     def __str__(self):
         return self.username
