d24ecaf134c3e909e9b4d116d761510e3fa8fed1
ramon <lionlightheart@gmail.com>
2024-09-20 21:22:43 +0200

    Refactor project structure and add roles app

--- pizzaForYouBackEnd/roles/views.py
+++ pizzaForYouBackEnd/roles/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Role
+from .serializers import RoleSerializer
 
-# Create your views here.
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
